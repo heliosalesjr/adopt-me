d695cce510d57cc8c1aa619cdf561acfd8635047
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-11-18 10:43:19 -0300

    eslint removed

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,21 @@
-{
-  
-}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"], // Ignorar apenas o necessário
+  rules: {
+    // Suas regras personalizadas
+  },
+};
