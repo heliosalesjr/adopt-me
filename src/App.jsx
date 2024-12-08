98c198a00b38904aa77b599e023e1367cb42dbae
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-12-08 20:38:49 -0300

    rendering the animal info

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 
 const App = () => {
   return (
     <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mix" />
-</div>);
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>);
 };
 
 const container = document.getElementById("root");
